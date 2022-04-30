f0e048868f2e619615bfbd3fbf625b2353d8c846
witerowson <runurowson@gmail.com>
2022-04-30 20:32:11 +0600

    third section

---
+++ src/Shared/Opinion.js
@@ -0,0 +1,57 @@
+
+import React from 'react';
+import { Carousel, CarouselItem } from 'react-bootstrap';
+import man1 from '../images/p1.png'
+import woman from '../images/p2.png'
+import man3 from '../images/p3.png'
+const Opinion = () => {
+    return (
+        <Carousel style={{ backgroundColor: '#110001' }} className='container text-light text-center p-5 rounded'>
+            <Carousel.Item className='text-center '>
+
+                <div className=''>
+                    <img
+                        style={{ width: '102px', height: '102px', borderRadius: '70px' }}
+                        src={man1}
+                        alt="first slide"
+                    />
+                    <p >One of the best site to belive words with their works</p>
+                    <h6>Product manager</h6>
+                    <p>Of Olive</p>
+
+                </div>
+            </Carousel.Item>
+            <Carousel.Item className='text-center '>
+
+                <div className=''>
+                    <img
+                        style={{ width: '102px', height: '102px', borderRadius: '70px' }}
+                        src={woman}
+                        alt="second slide"
+                    />
+                    <p >One of the best site to belive words with their works</p>
+                    <h6>Owner</h6>
+                    <p>Of Dinago</p>
+
+                </div>
+            </Carousel.Item>
+            <Carousel.Item className='text-center '>
+
+                <div className=''>
+                    <img
+                        style={{ width: '102px', height: '102px', borderRadius: '70px' }}
+                        src={man3}
+                        alt="Third slide"
+                    />
+                    <p >One of the best site to belive words with their works</p>
+                    <h6>Online Marketer</h6>
+                    <p>Of Olive</p>
+
+                </div>
+            </Carousel.Item>
+
+        </Carousel>
+    );
+};
+
+export default Opinion;
--- src/pages/HomePage/Home/Home.js
+++ src/pages/HomePage/Home/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import banner from '../../../images/b.png'
 import Items from '../../../Shared/itemsOfInventory/Items';
+import Opinion from '../../../Shared/Opinion';
 import UseInventory from '../../../Shared/UseInventory';
 const Home = () => {
     const [inventory, setInventory] = UseInventory()
     return (
-
-        <div >
+        <div className='' >
             <img className='w-100  ' src={banner} alt="" />
-
             <div className='container'>
-                <h2 style={{ color: '#110001' }} className='container font-roboto fs-1 text-center '>Our Best Collections</h2>
+                <h2 style={{ color: '#110001' }} className='container mt-5 font-roboto fs-1 text-center '>Our Best Collections</h2>
                 <div className='row'>
                     {inventory.map(item => <Items
                         key={item._id}
                         item={item}></Items>)}
 
                 </div>
+            </div>
+            <div>
+                <h2 style={{ color: '#110001' }} className='container font-roboto fs-1 text-center '>Our Partner's Opinions</h2>
+                <Opinion></Opinion>
             </div>
 
         </div>

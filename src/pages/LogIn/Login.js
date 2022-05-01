a934b1171b73b5c5450025434c57a995ef5716dd
witerowson <runurowson@gmail.com>
2022-05-01 20:07:19 +0600

    login

--- src/pages/LogIn/Login.js
+++ src/pages/LogIn/Login.js
@@ -1,26 +1,23 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+
 
 const Login = () => {
     const emailRef = useRef('')
     const passwordRef = useRef('')
     const navigate = useNavigate()
-
-
-    const navigateRegister = e => {
-        navigate('/register')
-    }
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
-        registered
     ] = useSignInWithEmailAndPassword(auth)
-
+    if (user) {
+        navigate('/home');
+    }
     const handleLogin = e => {
         e.preventDefault()
         const email = emailRef.current.value;
@@ -28,19 +25,21 @@ const Login = () => {
         signInWithEmailAndPassword(email, password)
         console.log(email, password);
     }
-
+    const navigateRegister = e => {
+        navigate('/register')
+    }
     return (
         <div style={{ color: '#110001' }} className='container w-50'>
             <h1 className='text-center' >Please Log In </h1>
             <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" name="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" name="password" placeholder="Password" />
                 </Form.Group>
                 <Button className='w-100' variant="danger" type="submit">
                     Log in

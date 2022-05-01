212d231ccf780eb18da67606a89d5dd061cd2ab4
witerowson <runurowson@gmail.com>
2022-05-01 10:43:28 +0600

    footer

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ImTwitter } from "react-icons/im"
+import { ImYoutube } from "react-icons/im"
+import { SiFacebook } from "react-icons/si"
 
 const Footer = () => {
     return (
-        <div style={{ backgroundColor: '#110001' }} className=' text-center mt-5'>
-            <p className='text-light'>
-                ©  2010-2022  All rights reserved.</p>
+        <div style={{ backgroundColor: '#110001' }} className=' text-center mt-5 p-3'>
+            <p className='text-light '>
+                Follow Us on <br /> <ImYoutube></ImYoutube>   <SiFacebook></SiFacebook>   <ImTwitter></ImTwitter> </p>
+            <p className='text-light mt-5'>
+                ©  2010-2022 PUre Perfume All rights reserved.</p>
         </div>
     );
 };
